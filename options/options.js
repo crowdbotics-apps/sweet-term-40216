79316e2b1e887c128629d79919778634e44d1ba3
Blake Wishard <blake.wishard@gmail.com>
2023-04-25 17:42:56 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "sweet-term-40216",
   url: "https://sweet-term-40216.botics.co",
   api: "https://sweet-term-40216.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
